92ef840523609228bc9f185756ceb11a5b647dbf
ChenhuaFan <805901483@qq.com>
2018-11-03 16:37:34 +0800

    test

--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,11 @@
 const Base64 = require('js-base64').Base64
 import http from '../util/http'
 
+/*
+user 用于记录当前登录用户的信息，可能是 student，teacher 或 admin。
+因此在此记录所有用户可能存在的信息
+*/
+
 const state = {
   'current': {},
   'jwt': "",
